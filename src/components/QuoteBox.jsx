d85d203f820d3c4d0d538e7c2afc1160428082bc
2DavidGarcia4 <gacub20ru@gmail.com>
2022-07-22 21:26:14 -0500

    update

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from 'react'
 import Random from "./Random";
 import quotes from "../quotes.json"
-let colors = ["#F47340", "#EF2F88", "#8843F2", "#F9D371", "#4700D8", "#211717", "#A34A28", "#FF6A38", "#F9CE00", "#00818A", "#B90B0B", "#4BA54D", "#1FA8CF", "#32FF6A", "#AD8B73", "#1D0251", "#E11D74", "#7FB414", "#A287F4", "#E71414"]
+let colors = ["#F47340", "#EF2F88", "#ac43f2", "#f8cb58", "#4700D8", "#211717", "#7c3316", "#d45806", "#F9CE00", "#00818A", "#b10303", "#318a65", "#1FA8CF", "#32FF6A", "#AD8B73", "#1D0251", "#E11D74", "#6ea009", "#A287F4", "#E71414"]
 
 function QuoteBox(){
    const posQ = Random(quotes), posC = Random(colors)
